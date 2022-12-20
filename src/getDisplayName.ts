c4e6f8f6307cec57a253f64e66368735511b56c7
dldvlpr <[dormoy.dev@gmail.com](mailto:dormoy.dev@gmail.com)>
2022-12-20 08:51:06 +0100

    fix false last push

--- src/getDisplayName.ts
+++ src/getDisplayName.ts
@@ -1,7 +1,5 @@
-const getDisplayName = (firstName: string, lastName: string, city: string = '', curriculum: string = '') => {
-    const mayBeCity = city || '?'
-    const mayBeWns = curriculum === 'workstudy' ? ' - WnS' : ''
-    return `[${mayBeCity}] ${firstName} ${lastName}${mayBeWns}`
+const getDisplayName = (firstName: string, lastName: string, city: string = '') => {
+    return ''
 }
 
 export default getDisplayName
